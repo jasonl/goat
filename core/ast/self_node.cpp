972c6941a4631b91a321217b3ad71b879977d32a
Jason Langenauer <jason@jasonlangenauer.com>
2010-11-21 20:48:56 +1100

    Added code generation for self node

--- core/ast/self_node.cpp
+++ core/ast/self_node.cpp
@@ -2,3 +2,32 @@
 
 SelfNode::SelfNode() : ASTNode( ASTNode::Self ) {
 }
+
+
+void SelfNode::Analyse( Scope *_scope ) {
+  scope = _scope;
+}
+
+AssemblyBlock *SelfNode::GenerateCode() {
+  AssemblyBlock *a = new AssemblyBlock;
+
+  a->MOV( eax, scope->GeneratePayloadOperand("self") );
+  a->MOV( ecx, scope->GenerateTypeHashOperand("self") );
+  a->MOV( edx, scope->GenerateDispatchOperand("self") );
+
+  a->CommentLastInstruction("Move self into eax/ecx/edx");
+
+  return a;
+}
+
+AssemblyBlock *SelfNode::PushOntoStack() {
+  AssemblyBlock *a = new AssemblyBlock;
+
+  a->PUSH( Dword(scope->GenerateTypeHashOperand("self")) );
+  a->PUSH( Dword(scope->GenerateDispatchOperand("self")) );
+  a->PUSH( Dword(scope->GeneratePayloadOperand("self")) );
+
+  a->CommentLastInstruction("Push self onto stack");
+
+  return a;
+}
--- core/ast/self_node.h
+++ core/ast/self_node.h
@@ -1,4 +1,7 @@
 class SelfNode : public ASTNode {
 public:
   SelfNode();
+  void Analyse( Scope * );
+  AssemblyBlock *GenerateCode();
+  AssemblyBlock *PushOntoStack();
 };
