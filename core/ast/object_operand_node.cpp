9a320aa89d0dc5a2ec577be02bc0efac69935fe0
Jason Langenauer <jason@jasonlangenauer.com>
2011-03-12 10:31:15 +1000

    Generate assembly for object operands in inline assembly

--- core/ast/object_operand_node.cpp
+++ core/ast/object_operand_node.cpp
@@ -1,5 +1,6 @@
+#include "../goat.hpp"
 #include "../ast_node.h"
-#include "../lexer.h"
+#include "../lexer.h" 
 
 ObjectOperandNode::ObjectOperandNode( TokenIterator &_token ) : OperandNode(ASTNode::ObjectOperand) {
   token = &(*_token);
@@ -10,5 +11,22 @@ void ObjectOperandNode::SetPropertyName( std::string _propertyName ) {
 }
 
 Operand *ObjectOperandNode::GenerateOperand() {
+  if(!scope->HasVariable(token->Content())) {
+    goatError(0, "No variable found in local scope");
+  }
+
+  if(propertyName == "payload") {
+    return &(scope->GeneratePayloadOperand(token->Content()));
+  }
+
+  if(propertyName == "dispatch") {
+    return &(scope->GenerateDispatchOperand(token->Content()));
+  }
+
+  if(propertyName == "type") {
+    return &(scope->GenerateTypeHashOperand(token->Content()));
+  }
+
+  goatError(0, "Invalid object property found in inline assembly");
   return NULL;
 }
