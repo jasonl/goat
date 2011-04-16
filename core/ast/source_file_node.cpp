db3961ad5f35f5034ea5bc060becbee6c7c1c4cc
Jason Langenauer <jason@jasonlangenauer.com>
2011-04-16 16:34:40 +0100

    Add start label for linker

--- core/ast/source_file_node.cpp
+++ core/ast/source_file_node.cpp
@@ -39,16 +39,22 @@ AssemblyBlock *SourceFileNode::GenerateCode() {
 
   if(!scope->GetSourceFile()->IsLibrary()) {
     // Call __Global__#main to start things
+    // Add a "start" label for the linker as an entry point
+    a->AppendItem(new GlobalSymbol("start"));
+
     a->mov(eax, Dword(0));
+    a->LabelLastInstruction("start");
+
+
     a->mov(ecx, Dword(goatHash("main")));
-    a->mov(edx, *new Operand(DispatchLabelNameFor("__GLOBAL__"))); 
+    a->mov(edx, *new Operand(DispatchLabelNameFor("__GLOBAL__")));
     a->call(edx);
     // Default exit code
     a->push( Dword(0) ); // Return exit code of 0
     a->mov(eax, *new Operand(0x01)); // System call number 1 - exit program
     a->sub(esp, *new Operand(0x04)); // OSX / BSD requires extra space on stack
     a->_int(*new Operand(0x80));  // Make the system call
-    
+
     a->CommentLastInstruction("Default exit back to system");
   }
 
